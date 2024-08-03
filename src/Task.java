f2335fceed5f1becde4c4dfb30bde219d5665aec
Anna <anlipios@yandex.ru>
2024-08-03 16:28:42 +0700

    1

---
+++ src/Task.java
@@ -0,0 +1,7 @@
+public class Task {
+        public static void printThreeWords(String[] args){
+            System.out.println("Orange");
+            System.out.println("Banana");
+            System.out.println("Apple");
+        }
+}
